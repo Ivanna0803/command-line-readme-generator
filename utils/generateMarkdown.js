6558753c2a10fbac238461ddf75424593e8b3b75
Ivanna <ivanna.kuliak@gmail.com>
2023-02-20 23:27:44 +0000

    Added functions that generate README file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.projectTitle}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributing 
+  ${data.contributing}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  ${data.username}
+  ${data.email}
 `;
 }
 
